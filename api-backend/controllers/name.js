c8cdeba03c977350543b9c85ad7bc036bd4aff87
Chris <el20227@mail.ntua.gr>
2024-02-07 14:05:53 +0200

    prettier error handling

--- api-backend/controllers/name.js
+++ api-backend/controllers/name.js
@@ -2,138 +2,68 @@ const mysql = require("mysql2");
 const { pool } = require('../utils/database');
 const {body} = require("express/lib/request");
 const axios = require('axios');
-const { Parser } = require('json2csv');
 
 const csv = require('csv-parser');
 const fs = require('fs');
-const e = require("cors");
 
 exports.getPersonDetails = async (req, res, err) => {
-    let format = req.query.format || 'json';
-
-    if(!(format === 'json' || format === 'csv') || format === '') {
-        const message = { message: 'Invalid format parameter! format should be json or csv', error: err ? err : '' };
-        return res.status(400).send(message);
+    let limit = undefined;
+    if (req.query.limit) {
+        limit = Number(req.query.limit);
+        if (!Number.isInteger(limit)) return res.status(400).json({ message: 'Limit query param should be an integer' });
     }
-    
+
     // status 400(Bad request) error handling
-    if (!req.params.nameID){
-        const message = { message: 'nameID is required', error: err ? err : '' };
-        if(format === 'json') 
-            return res.status(400).json(message);
-        else{
-            const json2csvParser = new Parser();
-            const csv = json2csvParser.parse(message);
-            res.setHeader('Content-Type', 'text/csv');
-            res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-            return res.status(400).send(csv);
-        }
-    }
+    if (req.params.nameID === undefined) return res.status(400).json({ message: 'nameID is required' });
     if (req.params.nameID[0] !== 'n' || req.params.nameID[1] !== 'm') {
-        const message = { message: 'Invalid nameID parameter! namedID should start with nm', error: err ? err : '' };
-        if(format === 'json') 
-            return res.status(400).json(message);
-        else{
-            const json2csvParser = new Parser();
-            const csv = json2csvParser.parse(message);
-            res.setHeader('Content-Type', 'text/csv');
-            res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-            return res.status(400).send(csv)
-        }
+        return res.status(400).json({ message: 'Invalid nameID parameter! namedID should start with nm', error: err ? err : ''});
     }
-
+    if (req.params.nameID.length !== 9) {
+        return res.status(400).json({ message: 'Invalid nameID parameter! namedID should have 9 characters', error: err? err : ''});
+    }
     const nameID = req.params.nameID;
     
+    
     const query = `
-
     SELECT
         p.nconst as nameID, 
-        COALESCE(p.primaryName,'') as name,
-        COALESCE(p.img_url_asset,'') as namePoster,
-        CASE 
-            WHEN p.birthYear = '0000' THEN ''
-            ELSE p.birthYear
-        END as birthYear,
-        CASE 
-            WHEN p.deathYear = '0000' THEN ''
-            ELSE p.deathYear
-        END as deathYear,
-        COALESCE(pr.category,'') as category,
-        COALESCE(pr.tconst,'') as titleID,
-        COALESCE(pp.primaryProfession,'') as professions
+        p.primaryName as name,
+        p.img_url_asset as namePoster,
+        p.birthYear as birthYr,
+        p.deathYear as deathYr,
+        pr.category as category,
+        pr.tconst as titleID,
+        pp.primaryProfession as professions
     FROM
         people p
-        LEFT JOIN principals pr ON p.nconst = pr.nconst
-        LEFT JOIN primaryprofession pp on pp.nconst = p.nconst
+        JOIN principals pr ON p.nconst = pr.nconst
+        JOIN primaryprofession pp on pp.nconst = p.nconst
     WHERE
-        p.nconst = ?`;
+        p.nconst = '${nameID}'` + (limit ? ' LIMIT ?' : '');
 
-    const queryParams = `${nameID}`;
+    const queryParams = [nameID];
+
+    if (limit !== undefined) {
+        queryParams.push(limit);
+    }
 
     pool.getConnection((err, connection) => {
-        if (err) {
-            const message = { message: 'Error in connection to the database', error: err ? err : '' };
-            if(format === 'json') 
-                return res.status(500).json(message);
-            else{
-                const json2csvParser = new Parser();
-                const csv = json2csvParser.parse(message);
-                res.setHeader('Content-Type', 'text/csv');
-                res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                return res.status(500).send(csv)
-            }    
-        }
+        if (err) return res.status(500).json({ message: 'Error in connection to the database' });
+
         connection.query(query, queryParams, (err, rows) => {
             connection.release();
 
-            if (err){ 
-                const message = { message: 'Error in executing the query' };
-                if(format === 'json') 
-                    return res.status(500).json(message);
-                else{
-                    const json2csvParser = new Parser();
-                    const csv = json2csvParser.parse(message);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                    return res.status(500).send(csv)
-                }  
-            }
+            if (err) return res.status(500).json({ message: 'Error in executing the query' });
             //status 204(no data) error handling
             if (rows.length === 0) {
-                const message = { message: 'No results found' };
-                if(format === 'json') 
-                    return res.status(204).json(message);
-                else{
-                    const json2csvParser = new Parser();
-                    const csv = json2csvParser.parse(message);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                    return res.status(204).send(csv)
-                }  
+                return res.status(204).json({ message: 'No results found' });
             }
             try {
                 const nameObject = processPersonResults(rows);
-                if(format === 'json') 
-                    return res.status(200).json(nameObject);
-                else{
-                    const json2csvParser = new Parser();
-                    const csv = json2csvParser.parse(nameObject);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                    return res.status(200).send(csv)
-                }
+                res.status(200).json({nameObject});
             }
             catch (error) {
-                const message = { message: 'Error processing person details', error };
-                if(format === 'json') 
-                    return res.status(204).json(message);
-                else{
-                    const json2csvParser = new Parser();
-                    const csv = json2csvParser.parse(message);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                    return res.status(204).send(csv)
-                }  
+                return res.status(500).json({ message: 'Error processing person details', error });
             }
         });
     });
@@ -170,54 +100,14 @@ function processPersonResults(results) {
 exports.getSearchPersonByName = async (req, res, err) => {
     let limit = undefined;
     let offset = undefined;
-    let format = req.query.format || 'json';
-
-    if(!(format === 'json' || format === 'csv')) {
-        const message = { message: 'Invalid format parameter! format should be json or csv', error: err ? err : '' };
-        return res.status(400).send(message);
-    }
-
 
     if (req.query.limit) {
         limit = Number(req.query.limit);
-        if (!Number.isInteger(limit)){
-            const message = { message: 'Limit query param should be an integer' , error : err ? err : ''};
-            if(format === 'json') 
-                return res.status(400).json(message);
-            else{
-                const json2csvParser = new Parser();
-                const csv = json2csvParser.parse(message);
-                res.setHeader('Content-Type', 'text/csv');
-                res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                return res.status(400).send(csv)
-            }
-        }
+        if (!Number.isInteger(limit)) return res.status(400).json({ message: 'Limit query param should be an integer' , error :err ? err : ''});
     }
     // status 400(Bad request) error handling
-    if (req.body.nqueryObject === undefined){
-        const message = { message: 'nqueryObject is required' , error : err ? err : ''};
-        if(format === 'json')
-            return res.status(400).json(message);
-        else{
-            const json2csvParser = new Parser();
-            const csv = json2csvParser.parse(message);
-            res.setHeader('Content-Type', 'text/csv');
-            res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-            return res.status(400).send(csv)
-        }
-    }
-    if (req.body.nqueryObject.namePart === undefined){
-        const message = { message: 'namePart is required' , error : err ? err : ''};
-        if(format === 'json')
-            return res.status(400).json(message);
-        else{
-            const json2csvParser = new Parser();
-            const csv = json2csvParser.parse(message);
-            res.setHeader('Content-Type', 'text/csv');
-            res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-            return res.status(400).send(csv)
-        }
-    }     
+    if (req.body.nqueryObject === undefined) return res.status(400).json({ message: 'nqueryObject is required' , error : err ? err : ''});
+    if (req.body.nqueryObject.namePart === undefined) return res.status(400).json({ message: 'namePart is required' , error : err ? err : ''});
     const namePart = req.body.nqueryObject.namePart;
 
     let page = req.query.page;
@@ -244,93 +134,50 @@ exports.getSearchPersonByName = async (req, res, err) => {
 
     if (offset !== undefined) {
         queryParams.push(offset);
-    } 
+    }
+    console.log(queryParams);   
 
     pool.getConnection((err, connection) => {
-        if (err){
-            const message = { message: 'Error in connection to the database' };
-            if(format === 'json')
-                return res.status(500).json(message);
-            else{
-                const json2csvParser = new Parser();
-                const csv = json2csvParser.parse(message);
-                res.setHeader('Content-Type', 'text/csv');
-                res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                return res.status(500).send(csv)
-            }
-        }    
-        connection.query(query, queryParams, async (err, rows) => {
+        if (err) return res.status(500).json({ message: 'Error in connection to the database' });
+
+        connection.query(query, queryParams, (err, rows) => {
             connection.release();
 
-            if (err){
-                const message = { message: 'Error in executing the query' , error: err ? err : ''};
-                if(format === 'json')
-                    return res.status(500).json(message);
-                else{
-                    const json2csvParser = new Parser();
-                    const csv = json2csvParser.parse(message);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                    return res.status(500).send(csv)
-                }
-            } 
+            if (err) return res.status(500).json({ message: 'Error in executing the query' , error: err ? err : ''});
 
             // status 204(no data) error handling
             if (rows.length === 0) {
                 // Return a 204 No Content status if there are no results
-                const message = { message: 'No results found', error: err ? err : ''};
-                if(format === 'json')
-                    return res.status(204).json(message);
-                else{
-                    const json2csvParser = new Parser();
-                    const csv = json2csvParser.parse(message);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                    return res.status(204).send(csv)
-                }
+                return res.status(204).json({ message: 'No results found', error: err ? err : ''});
             }
             
             // Map over the nconst values and call getPersonDetails for each one
-
-            const nconst = rows.map(row => row.nconst);
+            const nameDetailsPromises = rows.map(row => getPersonDetails(row.nconst).catch(error => {
+                console.error('Error fetching person details for', row.nconst);
+                return null; // return null if there's an error
+            }));
             const nameObjects = [];
 
-            try {
-                for (const n of nconst) {
-                    const personDetails = await getPersonDetails(n);
-                    nameObjects.push(personDetails);
-                }
-                if(format === 'json')
-                    return res.status(200).json(nameObjects);
-                else{
-                    const json2csvParser = new Parser();
-                    const csv = json2csvParser.parse(nameObjects);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                    return res.status(200).send(csv);
-                }
-            } catch (error) {
-                const message = { message: 'Error processing name details', error: error };
-                if(format === 'json')
-                    return res.status(500).json(message);
-                else{
-                    const json2csvParser = new Parser();
-                    const csv = json2csvParser.parse(message);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-                    return res.status(500).send(csv)
-                }
-            }
+            // Use Promise.all to wait for all the getPersonDetails requests to complete
+            Promise.all(nameDetailsPromises)
+                .then(nameObjects => {
+                    // Return the array of person details in the response
+                    res.status(200).json(nameObjects);
+                })
+                .catch(error => {
+                    return res.status(500).json({ message: 'Error processing person details!', error });
+                });
         });
     });
 };
 
+// Function to get person details based on nameID using the getPersonDetails endpoint
 async function getPersonDetails(nconst) {
-  try {
-    const response = await  axios.get(`http://localhost:9876/ntuaflix_api/name/${nconst}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching name details for', nconst, error.message);
-    throw error;
-  }
+    try {
+        const response = await axios.get(`http://localhost:9876/ntuaflix_api/name/${nconst}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching name details for', nconst, error.message);
+        throw error;
+    }
 }
