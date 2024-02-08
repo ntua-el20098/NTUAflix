c6ec0f11f16c0f3d4b244cc56688b1028be93da9
Dion Adam <dionadam2013@gmail.com>
2024-02-08 19:57:12 +0200

    modified:   api-backend/test/test.js

--- api-backend/test/test.js
+++ api-backend/test/test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 
 const nameID = 'nm0615736';
 
-const expected_json_return = {"name": "Musidora", "nameID": "nm0615736", "namePoster": "https://image.tmdb.org/t/p/{width_variable}/kHiLG0lyy5WqqxsLgYPAi61R37p.jpg", "nameTitles": [{"category": "self", "titleID": "tt0015414"}], "profession": "actress,director,writer"}
+const expected_json_return = {"nameObject":{"nameID":"nm0615736","name":"Musidora","namePoster":"https://image.tmdb.org/t/p/{width_variable}/kHiLG0lyy5WqqxsLgYPAi61R37p.jpg","birthYr":0,"deathYr":1957,"profession":"actress,director,writer","nameTitles":[{"titleID":"tt0015414","category":"self"}]}}
 
 describe('Test (GET Request: {baseurl}/name/:nameID)', () => {
     it('Should return data with status 200', (done) => {
