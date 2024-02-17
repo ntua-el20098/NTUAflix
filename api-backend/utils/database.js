13ae01cbcf49c3d45d680f3693887ccd3b8acf91
Chris <el20227@mail.ntua.gr>
2024-02-18 00:58:03 +0200

    fixing .env parameters

--- api-backend/utils/database.js
+++ api-backend/utils/database.js
@@ -3,11 +3,11 @@ const { env } = require("process");
 
 /* create connection and export it */
 const pool = mysql.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'tl',
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
 });
 
 module.exports = { pool };
